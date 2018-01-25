5953d861436fda67bc8c14534db2f513464cab32
Subash Karki <karki011@gmail.com>
2018-01-25 10:10:25 -0800

    bare-bones code

--- User-method/Program.cs
+++ User-method/Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 
-namespace User_method
+namespace AccountHolder
 {
-    class Users
+    class User
     {
-        public string FirstName { get ; set; }
-        public string LastName { get ; set; }
-        public int  Email { get; set; }
+        public static void Main()
+        {
+            string firstName;
+            string lastName;
+            bool isAnAccountOwner;
+            int loginPin;
 
-        
+            Console.Write("Please enter your first name: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("Please enter your last name: ");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Thank you, {0} {1}.", firstName, lastName);
+            Console.ReadLine();
+
+            Console.Write("Are you an Account holder?");
+            Console.ReadLine();
+        }
     }
+
+   
 }
