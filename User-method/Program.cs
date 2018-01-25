8d166a4806cb8eda3ad368006cef695194c951ad
Subash Karki <karki011@gmail.com>
2018-01-25 07:17:34 -0800

    basic code

---
+++ User-method/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace User_method
+{
+    class Users
+    {
+        public string FirstName { get ; set; }
+        public string LastName { get ; set; }
+        public int  Email { get; set; }
+
+        
+    }
+}
